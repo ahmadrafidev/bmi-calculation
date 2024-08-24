2a455096245d20d2370df1d6fcc5ac9328df8707
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-08-25 00:20:12 +0700

    feat: add dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import BMICalculator from '@/components/bmi-calculator'
+import DarkModeToggle from '@/components/ui/DarkModeToggle'
 
 export default function Home() {
   return (
     <main className="container mx-auto p-4 flex flex-col items-center justify-center min-h-screen">
+      <header className="p-3 absolute top-0 right-0">
+        <DarkModeToggle />
+      </header>
       <h1 className="text-2xl md:text-4xl font-bold font-sans tracking-tight mb-8 text-center">
         BMI Calculator App
       </h1>
